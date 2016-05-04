86b1110daf25fec6902501c0f5bd6548d7dd974e
jalexanderfox <jalexanderfox@gmail.com>
2016-05-03 23:23:30 -0400

    update docker_setEnv | check if machine running before setting env

--- docker/functions/setEnv.sh
+++ docker/functions/setEnv.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 #
 function docker_setEnv {
-  echo "=====docker-machine [$1]====="
-  if [[ "$1" == "vpn" ]]; then
-    eval "$($HOME/.dotfiles/docker/conf/docker-vpn-env.sh)"
+
+  if [[ $(docker-machine ls | grep Running | grep $1) ]]; then
+    $HOME/.dotfiles/.bash/contexts/actions/docker_setEnv.sh default
+
+    echo "=====docker-machine [$1]====="
+    if [[ "$1" == "vpn" ]]; then
+      eval "$($HOME/.dotfiles/docker/conf/docker-vpn-env.sh)"
+    else
+      eval "$(docker-machine env $1)"
+    fi
+
   else
-    eval "$(docker-machine env $1)"
+      echo "docker-machine default is not Running"
   fi
 }
